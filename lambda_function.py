0ed887ba66336598c16dd52dc7c423590a4b2819
AntoAndGar <antonioandgargiulo@gmail.com>
2022-07-23 17:54:28 +0200

    some adjustments

--- lambda_function.py
+++ lambda_function.py
@@ -1,39 +1,27 @@
+import json
+import requests
 from io import StringIO  # python3 (or BytesIO for python2)
 import boto3
-from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
+import os
 
 
 def lambda_handler(event, context):
+    base_url = "https://data.alpaca.markets/"
+    parameters = {"start": "2021-01-01T0:00:00Z", "timeframe": "1Day"}
 
-    api = REST("PKF2QMR530IM4SKCFZ68", "4BGfZRQbMFZ67U0x5M8bayTUgUcP1qpK6lfdq1Zd")
-    data = api.get_bars(
-        [
-            "AAPL",
-            "AMZN",
-            "GOOG",
-            "MSFT",
-            "TSLA",
-            "NFLX",
-            "META",
-            "BABA",
-            "TWTR",
-            "KO",
-            "NKE",
-            "PYPL",
-            "PEP",
-            "UBER",
-            "WMT",
-        ],
-        TimeFrame(1, TimeFrameUnit.Day),
-        "2021-01-01",
-        adjustment="raw",
-    ).df
+    headers = {
+        "Apca-Api-Key-Id": os.environ["ApcaApiKeyId"],
+        "Apca-Api-Secret-Key": os.environ["ApcaApiSecretKey"],
+    }
+    response = requests.get(
+        url=base_url + "v2/stocks/AAPL/bars?", headers=headers, params=parameters
+    )
 
-    bucket = "cloudcasa"  # already created on S3
-    csv_buffer = StringIO()
-    data.to_csv(csv_buffer)
+    csv_buffer = StringIO(str(response.json()))
 
     s3_resource = boto3.resource("s3")
-    s3_resource.Object(bucket, "df.csv").put(Body=csv_buffer.getvalue())
+    s3_resource.Object(os.environ["bucketName"], "AAPL.json").put(
+        Body=csv_buffer.getvalue()
+    )
 
     return {"statusCode": 200, "body": "OK"}
