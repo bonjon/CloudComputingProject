48c5dae1be8b8bc9eae9e069edc2022fff2a6eb0
AntoAndGar <antonioandgargiulo@gmail.com>
2022-07-24 15:32:54 +0200

    pparametric lambda

--- lambda_function.py
+++ lambda_function.py
@@ -6,22 +6,34 @@ import os
 
 
 def lambda_handler(event, context):
-    base_url = "https://data.alpaca.markets/"
-    parameters = {"start": "2021-01-01T0:00:00Z", "timeframe": "1Day"}
+    parameters = {"start": str(event["start"]), "timeframe": str(event["timeframe"])}
 
     headers = {
         "Apca-Api-Key-Id": os.environ["ApcaApiKeyId"],
         "Apca-Api-Secret-Key": os.environ["ApcaApiSecretKey"],
     }
     response = requests.get(
-        url=base_url + "v2/stocks/AAPL/bars?", headers=headers, params=parameters
+        url=str(event["base_url"])
+        + str(event["sub_url"])
+        + str(event["symbol"])
+        + str(event["query_url"]),
+        headers=headers,
+        params=parameters,
     )
 
     csv_buffer = StringIO(str(response.json()))
 
     s3_resource = boto3.resource("s3")
-    s3_resource.Object(os.environ["bucketName"], "AAPL.json").put(
+    s3_resource.Object(os.environ["bucketName"], str(event["symbol"]) + ".json").put(
         Body=csv_buffer.getvalue()
     )
 
-    return {"statusCode": 200, "body": "OK"}
+    return {
+        "statusCode": 200,
+        "body": "OK for query: "
+        + str(event["symbol"])
+        + " from:"
+        + str(event["start"])
+        + " in:"
+        + str(event["timeframe"]),
+    }
