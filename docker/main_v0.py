36fe11b6d72d0e85b95a764f733b008edf1b16af
AntoAndGar <antonioandgargiulo@gmail.com>
2022-07-26 19:21:50 +0200

    test read from s3

--- docker/main_v0.py
+++ docker/main_v0.py
@@ -14,12 +14,21 @@ install("streamlit-aggrid")
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import boto3
 
-bucket_name = "cloudcasa"
-s3 = boto3.resource("s3")
-bucket = s3.Bucket(bucket_name)
 
+API_KEY = "PKG95PV0R6DI0C2G522P"
+API_SECRET = "knWqSpYCGrkQopBig8K4dIWYNASADqzcjgalRx8O"
+BASE_URL = "https://data.alpaca.markets/"
+parameters = {
+    "start": "2021-10-01T0:00:00Z",
+    "timeframe": "1Day",
+    "symbols": "AAPL,TSLA,AMZN,META,NFLX",
+    "adjustment": "raw",
+}
+headers = {"Apca-Api-Key-Id": API_KEY, "Apca-Api-Secret-Key": API_SECRET}
+response = requests.get(
+    url=BASE_URL + "v2/stocks/bars?", headers=headers, params=parameters
+)
 st.title("Data Analytics")
 
 SYMBOLS = {
@@ -30,13 +39,6 @@ SYMBOLS = {
     "Netflix": "NFLX",
 }
 
-for obj in bucket.objects.all():
-    print("Nel bucket ci siamo arrivati")
-    print("obj.key:", obj.key)
-    if obj.key == "META.json":
-        json = obj.get()["Body"].read()
-        print(obj.get()["Body"].read())
-
 option = st.selectbox(
     "Select the Company Stock you want to see:",
     (SYMBOLS.keys()),
@@ -44,7 +46,7 @@ option = st.selectbox(
 
 st.write("You selected:", option)
 
-df = pd.DataFrame.from_dict(json["bars"][SYMBOLS[option]])
+df = pd.DataFrame.from_dict(response.json()["bars"][SYMBOLS[option]])
 fig = px.line(df, x="t", y="c", title=str(option) + " Stock")
 # fig.update_xaxes(type="category")
 fig.update_xaxes(
