4da513d1e13569caa44eb41e4db2809d2132f5e4
AntoAndGar <antonioandgargiulo@gmail.com>
2022-07-26 16:44:07 +0200

    comparison

--- docker/pages/comparison.py
+++ docker/pages/comparison.py
@@ -1,16 +1,4 @@
-import subprocess
-import sys
 import requests
-
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-install("streamlit")
-install("plotly")
-install("streamlit-aggrid")
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -39,16 +27,17 @@ SYMBOLS = {
     "Netflix": "NFLX",
 }
 
-option = st.selectbox(
-    "Select the Company Stock you want to see:",
-    (SYMBOLS.keys()),
-)
 
-st.write("You selected:", option)
+def get_data():
+    for sym, items in sorted(response.json()["bars"].items()):
+        for item in items:
+            item.update({"s": sym})
+            yield item
 
-df = pd.DataFrame.from_dict(response.json()["bars"][SYMBOLS[option]])
-fig = px.line(df, x="t", y="c", title=str(option) + " Stock")
-# fig.update_xaxes(type="category")
+
+df = pd.DataFrame.from_dict([elem for elem in get_data()])
+
+fig = px.line(df, x="t", y="c", color="s", title="Comparing Stock")
 fig.update_xaxes(
     rangeslider_visible=True,
     rangeselector=dict(
@@ -70,23 +59,5 @@ fig.update_layout(
     xaxis_rangeselector_activecolor="#626efb",
     xaxis_rangeselector_bgcolor="#262730",
 )
-# to hide the weeends and holidays
-# fig.update_xaxes(
-#    rangebreaks=[
-#        dict(bounds=["sat", "mon"]), #hide weekends
-#        dict(values=["2015-12-25", "2016-01-01"])  # hide Christmas and New #Year's
-#    ]
-# )
+
 st.plotly_chart(fig)
-
-max_closing_price = df["c"].max()
-st.metric(f"Highest Close price for {option}: ", round(max_closing_price, 4))
-
-max_date = df[df["c"] == max_closing_price]["t"].values[0]
-st.metric(f"Highest Close Price for {option} was observed on: ", max_date)
-
-min_closing_price = df["c"].min()
-st.metric(f"Lowest Close price for {option}: ", round(min_closing_price, 4))
-
-min_date = df[df["c"] == min_closing_price]["t"].values[0]
-st.metric(f"Lowest Close Price for {option} was observed on: ", min_date)
