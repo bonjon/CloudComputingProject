2821e523728e417c98656b481dde140f5e6edf7b
Elios Buzo <eliosbuzo@hotmail.it>
2022-07-28 19:04:59 +0200

    add logging

--- lambda_function_v2.py
+++ lambda_function_v2.py
@@ -1,22 +1,33 @@
 import boto3
 import json
+import logging
 import os
 import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     headers = {'APCA-API-KEY-ID' : os.environ['APCA_API_KEY_ID'], 'APCA-API-SECRET-KEY' : os.environ['APCA_API_SECRET_KEY']}
     parameters = {'start' : event['start'], 'timeframe' : event['timeframe']}
 
-    response = requests.get(event['base_url'] + event['sub_url'] + event['symbol'] + event['query_url'],
+    api_response = requests.get(event['base_url'] + event['sub_url'] + event['symbol'] + event['query_url'],
         headers = headers,
         params = parameters)
     
+    if api_response.status_code != 200:
+        logger.error('error' + str(api_response.status_code) + 
+                    'for API request' + event['base_url'] + 
+                    event['sub_url'] + event['symbol'] + event['query_url'])
+
     s3 = boto3.client('s3')
     bucket_name = os.environ['BUCKET_NAME']
     file_name = event['symbol'] + '.json'
-    body = json.dumps(response.json())
+    body = json.dumps(api_response.json())
     
-    s3.put_object(Body = body, Bucket = bucket_name, Key = file_name)
+    s3_response = s3.put_object(Body = body, Bucket = bucket_name, Key = file_name)
+    if s3_response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        logger.error('error' + s3_response['ResponseMetadata']['HTTPStatusCode'] + 
+                    'while uploading json to s3')
 
     return {
         "statusCode": 200,
