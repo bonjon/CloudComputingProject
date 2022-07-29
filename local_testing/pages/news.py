48c777f66aa61b3be1dc70fc221a3f10dec64526
bonjon <pica.1816394@studenti.uniroma1.it>
2022-07-29 23:29:25 +0200

    News Graphs

--- local_testing/pages/news.py
+++ local_testing/pages/news.py
@@ -1,4 +1,5 @@
-from os import truncate
+from email.base64mime import header_length
+from turtle import width
 import streamlit as st
 import plotly.express as px
 #from utils import *
@@ -6,14 +7,46 @@ import plotly.express as px
 import logging
 import requests
 import pandas as pd
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import numpy as np
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 st.title("Data Analytics")
-parameters = {"start": "2021-01-01T0:00:00Z", "symbols":"AAPL", "include_content": "true", "limit":"50", "sort": "ASC"}
+
+# Rest API
+parameters = {"start": "2022-07-01T0:00:00Z", "symbols": "AAPL,AMZN,META,NFLX,TSLA","limit":"50", "sort": "ASC"}
 headers = {"APCA-API-KEY-ID": "PKF2QMR530IM4SKCFZ68", "APCA-API-SECRET-KEY": "4BGfZRQbMFZ67U0x5M8bayTUgUcP1qpK6lfdq1Zd"}
 response = requests.get("https://data.alpaca.markets/v1beta1/news?", params=parameters, headers=headers)
+
+# Create the DataFrame for the News API
 df = pd.DataFrame.from_dict(response.json()["news"])
-fig = px.histogram(df, x="source")
+
+# Authors Plot
+fig = px.bar(df, x='author')
+st.subheader('Authors Plot')
 st.plotly_chart(fig)
+
+# Most Cited Symbols/Companies Plot
+st.subheader('Most Cited Symbols')
+join_df = np.array([i for i in df['symbols']])
+join_df = np.concatenate(join_df).ravel()
+fig = px.bar(join_df)
+fig.update_layout(showlegend=False)
+st.plotly_chart(fig)
+
+# Most Frequent Words Plot
+st.subheader('Most Frequent Words')
+text = " ".join(i for i in df['summary'])
+STOPWORDS = ["S"] + list(STOPWORDS)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text)
+print(text)
+plt.figure(figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.pyplot()
